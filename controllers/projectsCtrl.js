60a6d0f3b063aee13674914a8ce11403ef3669c0
AhKhamis <7mody.stacks@gmail.com>
2026-08-23 13:28:47 +0300

    Add project creation and viewing

--- controllers/projectsCtrl.js
+++ controllers/projectsCtrl.js
@@ -10,6 +10,37 @@ const index = async (req, res) => {
   }
 };
 
+const show = async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    res.render('projects/show.ejs', { project });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/projects');
+  }
+};
+
+const newProject = async (req, res) => {
+  res.render('projects/new.ejs');
+};
+
+const create = async (req, res) => {
+  try {
+    req.body.designer = req.session.user._id;
+
+    const project = await Project.create(req.body);
+
+    res.redirect(`/projects/${project._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/projects');
+  }
+};
+
 module.exports = {
   index,
+  show,
+  newProject,
+  create,
+
 };
--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const isSignedIn = require('../middleware/isSignedIn');
 const router = express.Router({ mergeParams: true });
 
 const projectsCtrl = require('../controllers/projectsCtrl');
 
 router.get('/', projectsCtrl.index);
+router.get('/new', isSignedIn, projectsCtrl.newProject);
+router.post('/', isSignedIn, projectsCtrl.create);
+router.get('/:id', projectsCtrl.show);
 
 module.exports = router;
