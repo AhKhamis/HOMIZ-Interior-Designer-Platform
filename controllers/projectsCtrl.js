1cff5bd740f24fe976fe5baa0ed88874fdd322f6
AhKhamis <7mody.stacks@gmail.com>
2026-08-24 00:23:56 +0300

    Add project services and add delete button

--- controllers/projectsCtrl.js
+++ controllers/projectsCtrl.js
@@ -1,4 +1,5 @@
 const Project = require('../models/project');
+const Service = require('../models/service');
 
 const index = async (req, res) => {
   try {
@@ -12,7 +13,7 @@ const index = async (req, res) => {
 
 const show = async (req, res) => {
   try {
-    const project = await Project.findById(req.params.id);
+    const project = await Project.findById(req.params.id).populate('services');
     res.render('projects/show.ejs', { project });
   } catch (err) {
     console.log(err);
@@ -21,12 +22,22 @@ const show = async (req, res) => {
 };
 
 const newProject = async (req, res) => {
-  res.render('projects/new.ejs');
+  try {
+    const services = await Service.find();
+    res.render('projects/new.ejs', { services });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/projects');
+  }
 };
 
 const create = async (req, res) => {
   try {
     req.body.designer = req.session.user._id;
+
+    if (req.body.services && !Array.isArray(req.body.services)) {
+      req.body.services = [req.body.services];
+    }
 
     const project = await Project.create(req.body);
 
@@ -39,10 +50,11 @@ const create = async (req, res) => {
 
 const edit = async (req, res) => {
   try {
-    const project = await Project.findById(req.params.id);
-    res.render('projects/edit.ejs', { project });
-  } catch (err) {
+    const project = await Project.findById(req.params.id).populate('services');
+    const services = await Service.find();
 
+    res.render('projects/edit.ejs', { project, services });
+  } catch (err) {
     console.log(err);
     res.redirect('/projects');
   }
@@ -50,6 +62,10 @@ const edit = async (req, res) => {
 
 const update = async (req, res) => {
   try {
+    if (req.body.services && !Array.isArray(req.body.services)) {
+      req.body.services = [req.body.services];
+    }
+
     const project = await Project.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -81,5 +97,5 @@ module.exports = {
   edit,
   update,
   deleteProject,
-  
+
 };
