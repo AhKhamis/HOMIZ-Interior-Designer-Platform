e7d446944b9994c759e78ae96bce9d29170bf0a9
AhKhamis <7mody.stacks@gmail.com>
2026-08-25 20:53:53 +0300

    Complete admin blog CRUD

--- controllers/blogCtrl.js
+++ controllers/blogCtrl.js
@@ -22,7 +22,64 @@ const show = async (req, res) => {
   }
 };
 
+const newBlog = async (req, res) => {
+  res.render('blog/new.ejs');
+};
+
+const create = async (req, res) => {
+  try {
+    await Blog.create(req.body);
+
+    res.redirect('/blog');
+  } catch (err) {
+    console.log(err);
+    res.redirect('/blog');
+  }
+};
+
+const edit = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+
+    res.render('blog/edit.ejs', { blog });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/blog');
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.redirect(`/blog/${blog._id}`);
+  } catch (err) {
+    console.log(err);
+    res.redirect('/blog');
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+
+    res.redirect('/blog');
+  } catch (err) {
+    console.log(err);
+    res.redirect('/blog');
+  }
+};
+
 module.exports = {
   index,
   show,
+  newBlog,
+  create,
+  edit,
+  update,
+  deleteBlog,
 };
--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const isAdmin = require('../middleware/isAdmin');
 const blogCtrl = require('../controllers/blogCtrl');
 
 const router = express.Router();
 
 router.get('/', blogCtrl.index);
+
+router.get('/new', isAdmin, blogCtrl.newBlog);
+router.post('/', isAdmin, blogCtrl.create);
+
+router.get('/:id/edit', isAdmin, blogCtrl.edit);
+router.put('/:id', isAdmin, blogCtrl.update);
+
+router.delete('/:id', isAdmin, blogCtrl.deleteBlog);
 
 router.get('/:id', blogCtrl.show);
 
